2c018e60ab53049174281c9bdbb81bd6d0800b4c
j4587698 <jx@jvxiang.com>
2026-06-01 09:59:01 +0800

    Fix missing using directive for IEnumerable

--- KuGouMusicAvalonia/KuGouMusicAvalonia/ViewModels/PlaylistDetailViewModel.cs
+++ KuGouMusicAvalonia/KuGouMusicAvalonia/ViewModels/PlaylistDetailViewModel.cs
@@ -2,6 +2,7 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using KuGou.Lite;
 using KuGouMusicAvalonia.Services;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
